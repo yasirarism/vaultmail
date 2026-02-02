96a7701381d86751f2121e7e333ba9b0af3773e7
Yasir Aris M <git@yasir.id>
2026-02-02 16:18:00 +0700

    Annotate DEFAULT_EMAIL as string to avoid TypeScript build error

--- lib/config.ts
+++ lib/config.ts
@@ -4,7 +4,7 @@ export const DEFAULT_DOMAINS: string[] = [];
 
 export const DEFAULT_DOMAIN = DEFAULT_DOMAIN_FALLBACK;
 
-export const DEFAULT_EMAIL = '';
+export const DEFAULT_EMAIL: string = '';
 
 export const getDefaultEmailDomain = () => {
   return DEFAULT_DOMAIN;
