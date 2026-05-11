50e345d7edbfeec016cd2b7f074ca16e730e764e
Yasir Aris M <git@yasir.id>
2026-05-11 15:11:19 +0700

    Advance IMAP last UID even when message is filtered out

--- lib/imap-fetch.ts
+++ lib/imap-fetch.ts
@@ -218,6 +218,7 @@ export const fetchFromImap = async (address: string, existingSourceIds: Set<stri
     let maxSeenUid = lastUid;
     for (const uid of ids) {
       const uidNum = Number(uid);
+      if (Number.isFinite(uidNum) && uidNum > maxSeenUid) maxSeenUid = uidNum;
       const res = await runImapCommand(
         socket,
         `f${uid}`,
@@ -252,7 +253,6 @@ export const fetchFromImap = async (address: string, existingSourceIds: Set<stri
         receivedAt: parseReceivedAt(headers.get('date')),
         read: false
       });
-      if (Number.isFinite(uidNum) && uidNum > maxSeenUid) maxSeenUid = uidNum;
     }
     if (maxSeenUid > lastUid) {
       await storage.set(lastUidKey(address), String(maxSeenUid));
